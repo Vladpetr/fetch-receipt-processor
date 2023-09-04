9c8c4a68adab3a553b0d8159995d02cb647c273c
unknown <vladislavzpua@gmail.com>
2023-09-04 10:32:34 -0500

    Dockerize the backend

--- receipt_processor/receipt_processor/settings.py
+++ receipt_processor/receipt_processor/settings.py
@@ -13,18 +13,16 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-load_dotenv()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(os.path.join(BASE_DIR, "django.env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
-
+print(SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
